1918eebbb3d5e9bad04a61b4eb2a7bf4ab004ece
Asma Ali <asmaali@Asmas-MacBook.local>
2020-03-19 22:11:02 -0400

    fixed function getInterview(state, interview)

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -7,11 +7,10 @@ export function getAppointmentsForDay(state, day) {
   return filteredAppointments;
 }
 
-export function getInterview(state, day) {
-  if (state.days.length === 0) return [];
-  if (!state.days.map(item => item.name).includes(day)) return [];
-  const filteredDays = state.days.filter(element => element.name === day)[0]
-    .interviewers;
-  const filteredAppointments = filteredDays.map(id => state.interviewers[id]);
-  return filteredAppointments;
+export function getInterview(state, interview) {
+  const interviewer = state.interviewers[interview.interviewer];
+  return {
+    ...interview,
+    interviewer
+  };
 }
