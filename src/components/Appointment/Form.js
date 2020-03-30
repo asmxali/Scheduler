269c31a03a9fab8d67c290ca89caacf1636d0774
Asma Ali <asmaali@Asmas-MacBook.local>
2020-03-29 23:16:58 -0400

    updated validate function to display an error when an interviewer isn't selected

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -17,6 +17,10 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
+    if (!interviewer) {
+      setError("An interviewer must be selected");
+      return;
+    }
     setError("");
     props.onSave(name, interviewer);
   }
